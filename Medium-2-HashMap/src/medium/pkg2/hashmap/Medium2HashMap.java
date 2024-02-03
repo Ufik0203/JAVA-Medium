d3cdc185a5b9d1ecb26656858a87a31245739ef4
Ufik <ufik0203@gmail.com>
2024-02-03 18:31:39 +0700

    commit

---
+++ Medium-2-HashMap/src/medium/pkg2/hashmap/Medium2HashMap.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package medium.pkg2.hashmap;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author mario
+ */
+public class Medium2HashMap {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private void hsmp1() {
+        HashMap map = new HashMap<>();
+        
+    }
+    
+    public static void main(String[] args) {
+        Medium2HashMap obj = new Medium2HashMap();
+    }
+    
+}
