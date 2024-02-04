f3c6a247fa3d290a951099875df2d56a3cd787cd
Ufik <ufik0203@gmail.com>
2024-02-04 22:12:01 +0700

    update commit

--- Medium-2-HashMap/src/medium/pkg2/hashmap/Medium2HashMap.java
+++ Medium-2-HashMap/src/medium/pkg2/hashmap/Medium2HashMap.java
@@ -27,7 +27,7 @@ public class Medium2HashMap {
         String input;
         
         do {
-            System.out.println("Pilihan : ");
+            System.out.println("\nPilihan : ");
             System.out.println("1. Input");
             System.out.println("2. Change/Update");
             System.out.println("3. Delete");
@@ -35,52 +35,173 @@ public class Medium2HashMap {
             System.out.print("Masukkan pilihan : ");
             a = scan.nextInt();
             scan.nextLine();
-            cls();  // Implement how tp cls on java
-            
-            switch(a) {
+            cls();  // Implement how tp cls on java "JUST WORK IN VS CODE"
+            switch (a) {
                 case 1:
                     System.out.println("\nPilih Inputan : ");
                     System.out.println("1. Auto key");    // Will give auto key input on hashmap
                     System.out.println("2. Manual key");    // Will give key input on hashmap when useer input the key
                     b = scan.nextInt();
                     scan.nextLine();
-                    
-                    switch(b) {
+
+                    switch (b) {
                         case 1:
                             System.out.print("Masukkan nilai Hashmap : ");
                             input = scan.nextLine();
                             map.put(auto++, input);
-                            System.out.println(map);
+                            System.out.println("\n" + map);
                             break;
                         case 2:
-                            System.out.print("Masukkan kunci Hashmap : ");
-                            c = scan.nextInt();
-                            scan.nextLine();
-                            
-                            System.out.print("Masukkan nilai Hashmap : ");
-                            input = scan.nextLine();
-                            map.put(c, input);
-                            System.out.println(map);
+                            boolean valid = false;
+
+                            while (!valid) {
+                                System.out.print("Masukkan kunci Hashmap : ");
+                                if (scan.hasNextInt()) {
+                                    c = scan.nextInt();
+                                    scan.nextLine();
+
+                                    System.out.print("Masukkan nilai Hashmap : ");
+                                    input = scan.nextLine();
+                                    map.put(c, input);
+                                    System.out.println("\n" + map);
+                                    valid = true;
+
+                                } else {
+                                    System.out.println("Inputan Kunci/Key harus berupa angka");
+                                    scan.nextLine();
+                                }
+                            }
                             break;
                         default:
                             System.out.println("Inputan tidak valid");
+                            break;
                     }
+                    break;
+                    
                 case 2:
-                    System.out.println(/);
-                
+                    int key,nwk;
+                    Map<Integer, String> newmp = new HashMap<>();
+                    boolean valid = false;
+                    if (!map.isEmpty()) {
+
+                        System.out.println("\n\nIsi Hashmap :");
+                        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+                            System.out.println("Key = " + entry.getKey() + " Value = " + entry.getValue());
+                        }
+
+                        System.out.println("\nIngin mengubah bagian apa ?");
+                        System.out.println("1. Key/kunci saja");
+                        System.out.println("2. Nilai/value saja");
+                        System.out.println("3. Semua");
+                        int choose = scan.nextInt();
+
+                        switch (choose) {
+                            case 1:
+                                while (!valid) {
+                                    System.out.print("\nMasukkan key yang ingin di ubah : ");
+                                    key = scan.nextInt();
+                                    scan.nextLine();
+
+                                    if (map.containsKey(key)) {
+                                        System.out.print("\nMasukkan key baru : ");
+                                        nwk = scan.nextInt();
+                                        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+                                            if (entry.getKey() == key) {
+                                                newmp.put(nwk, entry.getValue());
+                                            } else {
+                                                newmp.put(entry.getKey(), entry.getValue());
+                                            }
+                                        }
+                                        map.clear();
+                                        map.putAll(newmp);
+                                        valid = true;
+                                    } else {
+                                        System.out.println("Key tidak terdaftar!!!");
+                                    }
+                                }
+                                break;
+
+                            case 2:
+                                while (!valid) {
+                                    System.out.print("\nMasukkan key yang ingin di ubah : ");
+                                    key = scan.nextInt();
+                                    scan.nextLine();
+
+                                    if (map.containsKey(key)) {
+                                        System.out.print("\nMasukkan nilai yang baru : ");
+                                        String value = scan.nextLine();
+                                        map.replace(key, value);
+                                        valid = true;
+                                    } else {
+                                        System.out.println("Key tidak terdaftar!!!");
+                                    }
+                                }
+                                break;
+
+                            case 3:
+                                while (!valid) {
+                                    System.out.print("\nMasukkan key mana yang ingin di ubah semua : ");
+                                    key = scan.nextInt();
+                                    scan.nextLine();
+
+                                    if (map.containsKey(key)) {
+                                        System.out.print("\nMasukkan key baru : ");
+                                        nwk = scan.nextInt();
+                                        scan.nextLine();
+
+                                        System.out.print("\nMasukkan value baru : ");
+                                        input = scan.nextLine();
+
+                                        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+                                            if (entry.getKey() == key) {
+                                                newmp.put(nwk, input);
+                                            } else {
+                                                newmp.put(entry.getKey(), entry.getValue());
+                                            }
+                                        }
+                                        map.clear();
+                                        map.putAll(newmp);
+                                        valid = true;
+                                    } else {
+                                        System.out.println("Key tidak terdaftar!!!");
+                                    }
+                                }
+                                break;
+                        }
+                    } else {
+                        System.out.println("!!!!!!  Hashmap kosong  !!!!!!");
+                    }
+                    break;
+
                 case 3:
-                    sa
-                
+                    if (!map.isEmpty()) {
+                        valid = false;
+                        while (!valid) {
+                            System.out.println("Masukkan key mana yang ingin dihapus ? ");
+                            key = scan.nextInt();
+                            if (map.containsKey(key)) {
+                                map.remove(key);
+                                valid = true;
+                            } else {
+                                System.out.println("Data tidak terdaftar");
+                            }
+                        }
+                        break;
+                    } else {
+                        System.out.println("!!!!!!  Data kosong  !!!!!!!");
+                    }
+                    break;
+
                 case 4:
                     System.out.println("\nTerima kasih");
                     z = false;
                     break;
-                    
+
                 default:
                     System.out.println("Inputan tidak valid");
+                    break;
             }
-            
-        } while(z);
+        } while (z);
           
     }
     
