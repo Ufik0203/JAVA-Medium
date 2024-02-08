13d5bcf4ce9acb8c8f7aaebb07451ea3c9d4b247
Ufik <ufik0203@gmail.com>
2024-02-08 17:27:36 +0700

    update commit

--- Medium-5-Rekursif/src/medium/pkg5/rekursif/Medium5Rekursif.java
+++ Medium-5-Rekursif/src/medium/pkg5/rekursif/Medium5Rekursif.java
@@ -20,21 +20,63 @@ public class Medium5Rekursif {
         return a + b;
     }
     
+    private static boolean prima (int a) {
+        if (a <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
     private void rks() {
         Scanner scan = new Scanner(System.in);
-        int a;
+        int a,b,c;
         boolean valid = true;
-        
-        System.out.println("1. Cek ganjil/Genap");
-        System.out.println("2. Cek apakah bilangan prima/tidak");
-        System.out.println("Masukkan pilihan : ");
         do {
-            
+            System.out.println("\n\n1. Cek ganjil/Genap");
+            System.out.println("2. Cek apakah bilangan prima/tidak");
+            System.out.println("3. Keluar");
+            System.out.println("Masukkan pilihan : ");
+            a = scan.nextInt();
+            switch (a) {
+                case 1:
+                    System.out.println("Inputkan angka : ");
+                    b = scan.nextInt();
+                    if (b % 2 == 0) {
+                        System.out.println(" Genap");
+                    } else {
+                        System.out.println(" Ganjil");
+                    }
+                    
+                    a = 0;
+                break;
+                
+                case 2:
+                    System.out.println("Inputkan angka : ");
+                    c = scan.nextInt();
+                    if (prima(c)) {
+                        System.out.println(" Prima");
+                    } else {
+                        System.out.println(" Bukan prima");
+                    }
+                break;
+                
+                case 3:
+                    System.out.println("Terima kasih");
+                    valid = false;
+                    break;
+            }
         } while (valid);
     }
     
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Medium5Rekursif obj = new Medium5Rekursif();
+        
+        /*Scanner scan = new Scanner(System.in);
         int a,b,c;
         
         System.out.print("\nMasukkan nilai ke-1 : ");
@@ -44,7 +86,9 @@ public class Medium5Rekursif {
         b = scan.nextInt();
         
         c = tambah(a, b);
-        System.out.println("\nHasil nya adalah " + c);
+        System.out.println("\nHasil nya adalah " + c);*/
+        
+        obj.rks();
     }
     
 }
